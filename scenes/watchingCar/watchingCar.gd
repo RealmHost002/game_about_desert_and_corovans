561d955a1c08f12759d4648c8f17b7a58f5bb0ff
Spudi2007 <43494450+Spudi2007@users.noreply.github.com>
2020-09-03 20:18:13 +0300

    useless change
    adding folder with 2 empty scenese

---
+++ scenes/watchingCar/watchingCar.gd
@@ -0,0 +1,17 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var car
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#self.add_child("res://car.tscn")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
