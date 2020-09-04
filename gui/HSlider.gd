efe6c66f9a5a19c411a72620dc068ef762a866fd
Spudi2007 <43494450+Spudi2007@users.noreply.github.com>
2020-09-04 18:46:15 +0300

    slidersadd

---
+++ gui/HSlider.gd
@@ -0,0 +1,24 @@
+extends HSlider
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+
+
+
+func _on_HSlider_value_changed(value):
+	get_child(0).text = str(self.value) + "%"
+	pass # Replace with function body.
