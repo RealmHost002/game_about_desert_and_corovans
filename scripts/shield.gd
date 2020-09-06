9287f44c40730a003db85ef33f44b95b9f88a4c0
RealmHost002 <tukow@yandex.ru>
2020-09-06 15:41:06 +0300

    SHIELDANIMATIONS

---
+++ scripts/shield.gd
@@ -0,0 +1,16 @@
+extends Position3D
+
+var type = 'generator'
+var image_path = "res://icon.png"
+var is_pressable = false
+var have_slider = true
+var shield_production
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
