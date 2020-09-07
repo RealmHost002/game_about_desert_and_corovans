e7bab5037ac808ba07ebd68a1c8e2c18398cda27
Spudi2007 <43494450+Spudi2007@users.noreply.github.com>
2020-09-07 16:41:09 +0300

    gui labels

--- gui/GUI.gd
+++ gui/GUI.gd
@@ -40,8 +40,14 @@ func AddingWeaponButtons():
 		
 		counter += 1
 		
-	
-		
+func setting_car_params():
+	var labels_parent = get_node("HBoxContainer2/TextureButton")
+	if constants.selectedCar:
+		labels_parent.get_node("hp_label").text = "Hp: " + str(constants.selectedCar.hp)
+		labels_parent.get_node("energy_label").text = "Energy: " + str(constants.selectedCar.energy)		
+		labels_parent.get_node("shield_label").text = "Shield: " + str(constants.selectedCar.shield)	
+		labels_parent.get_node("energy_regen_label").text = "Energy regen: " + str(constants.selectedCar.energy_production)		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	setting_car_params()
+	pass
