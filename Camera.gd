24da7cc63c15705712cf09d9e5fc8fb0448c0e19
Spudi2007 <43494450+Spudi2007@users.noreply.github.com>
2020-10-23 21:49:26 +0300

    Revert "Revert "commit""
    This reverts commit 18b7d08a4e1977cd70ba7f274f4761a38b915b46.

--- Camera.gd
+++ Camera.gd
@@ -8,21 +8,35 @@ var drag_vertical = 0.0
 
 var drag_speed = 10.0
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass # Replace with function body.
 
 func _input(event):
+	if !current:
+		return
+		
 	if event is InputEventMouseMotion and drag:
 		get_parent().global_transform.origin += (get_node("../x").global_transform.origin - get_parent().global_transform.origin) * event.relative.x / anti_speed + (get_node("../z").global_transform.origin - get_parent().global_transform.origin) * event.relative.y / anti_speed
+	
 	
 	var close_up_vec = (self.global_transform.origin - get_parent().global_transform.origin).normalized()
 	if event.is_action_pressed('wheel_up'):
 #		self.fov -= 7.0
-		self.global_transform.origin -= close_up_vec
+		self.global_transform.origin -= close_up_vec * drag_speed / 10.0
 	if event.is_action_pressed('wheel_down'):
 #		self.fov += 7.0
-		self.global_transform.origin += close_up_vec
+		self.global_transform.origin += close_up_vec * drag_speed / 10.0
+		print((self.global_transform.origin - get_parent().global_transform.origin).length())
+	
+	#camera limits
+	if (self.global_transform.origin - get_parent().global_transform.origin).length() < pow(drag_speed, 1.3) * 0.14:
+		self.global_transform.origin += close_up_vec * drag_speed / 10.0
+	if (self.global_transform.origin - get_parent().global_transform.origin).length() > drag_speed * 1.1:
+		self.global_transform.origin -= close_up_vec * drag_speed / 10.0
+	
+	
+	
 	if event is InputEventMouseMotion:
 #		print(event.position.y)
 		if event.position.x == constants.machine_resolution.x - 1:
@@ -63,11 +77,11 @@ func _process(delta):
 	var pos_vec = get_parent().global_transform.origin - Vector3(10, 0, 10)
 	pos_vec.y = pos_vec.z
 	pos_vec.z = 0
-	get_node("../MeshInstance").get_surface_material(0).set_shader_param("uv1_offset", pos_vec / 20.0)
+	if !(get_parent().get_parent().name == 'map'):
+		get_node("../MeshInstance").get_surface_material(0).set_shader_param("uv1_offset", pos_vec / 20.0)
+		get_node("../StaticBody").global_transform.origin = Vector3(ceil(get_parent().global_transform.origin.x/20.0) * 20, 0, ceil(get_parent().global_transform.origin.z/20.0) * 20) - Vector3(10,0,10)
 	
 	get_parent().global_transform.origin.x += delta * drag_horizontal * drag_speed
 	get_parent().global_transform.origin.z += delta * drag_vertical * drag_speed
 	
-	
-	
-	
+#	get_node("../StaticBody").global_transform.origin = Vector3(ceil(get_parent().global_transform.origin.x/20.0) * 20, 0, ceil(get_parent().global_transform.origin.z/20.0) * 20) - Vector3(10,0,10)
